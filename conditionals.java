886b257b43502cfac41dd847856f53040f239a5d
Ceyda <122219025+ceydabezek@users.noreply.github.com>
2023-01-28 22:06:07 +0300

    Add files via upload

---
+++ conditionals.java
@@ -0,0 +1,16 @@
+public class conditionals {
+public static void main(String[] args) {
+    
+    int sayi = 24;
+    if(sayi<20) { 
+        System.out.println("Number less than 20");}
+    
+             else if(sayi ==20) {
+            System.out.println("Number equal to 20");
+
+        }
+        else {System.out.println("Number more than 20");}
+    
+
+}
+}
---
+++ variables.java
@@ -0,0 +1,11 @@
+public class variables {
+public static void main(String[] args) {
+    
+    int numberOfStudents= 12;
+    String message = "Number of students:";
+    System.out.println(message + numberOfStudents);
+    System.out.println(message + numberOfStudents);
+    System.out.println(message + numberOfStudents);
+}
+
+}
