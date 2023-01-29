14e37adb277971e0a7769a5e9b3857d98ea451d9
Ceyda <122219025+ceydabezek@users.noreply.github.com>
2023-01-29 13:56:35 +0300

    Add files via upload

---
+++ loops.java
@@ -0,0 +1,13 @@
+public class loops {
+    public static void main(String[] args) {
+        for (int i=1;i<=10;i++ //i+=2 (odd number)
+        ){
+            System.out.println(i);
+        }
+        System.out.println("Loop over");
+
+
+
+
+    }
+}
